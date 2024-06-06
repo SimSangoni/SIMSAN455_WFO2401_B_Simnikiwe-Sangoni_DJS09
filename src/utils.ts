39abc8d6f610da13d299b1561e3a5032152c0698
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-06 13:51:09 +0200

    updated loyaltyIcon based on enum

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { UserLoyalty } from "./enums";
+
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement;
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement;
@@ -6,10 +8,17 @@ const propertyDisplay = document.querySelector('.properties') as HTMLElement;
 
 
 
-export function totalReviews(review: number, viewer: string, loyal: boolean ){
-    const loyaltyStar = loyal? "⭐": ""
+export function totalReviews(review: number, viewer: string, loyal: UserLoyalty ){
+    let loyaltyIcon = ''
+    if (loyal === UserLoyalty.GOLD){
+        loyaltyIcon = '🥇'
+    } else if (loyal === UserLoyalty.BRONZE){
+        loyaltyIcon = '🥉'
+    } else {
+        loyaltyIcon = '🥈'
+    }
     reviewTotalDisplay.innerHTML = `review total  ${review.toString()} 
-    | last reviewed by ${viewer} ${loyaltyStar}`
+    | last reviewed by ${viewer} ${loyaltyIcon}`
 }
 
 export function populateUser(isReturning: boolean, userName: string ) {
