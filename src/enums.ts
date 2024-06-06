d9ec4aec6cb31667ab27a33538991c8d56abe0eb
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-06 13:51:58 +0200

    removed mini-challenge instructions

--- src/enums.ts
+++ src/enums.ts
@@ -8,10 +8,3 @@ export enum UserLoyalty {
     SILVER = 'SILVER_USER',
     BRONZE = 'BRONZE_USER'
 }
-
-// Enum Types mini-challenge
-// Replace the value of loyaltyUser to a GOLD_USER, SILVER_USER or BRONZE_USER, making sure to
-// use what we learnt about Enums in the previous lesson. Make Sheia GOLD, Andrzej BRONZE 
-// and Omar SILVER.
-// 2. export the enum
-// 3. Fix the function in the utils to show Sheias star as she is a GOLD_USER.
