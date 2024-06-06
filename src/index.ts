0eb7656a8f22db11aa9da6fa7b17e28d9d3af8ea
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-06 09:58:55 +0200

    properties array of objects

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { totalReviews, populateUser } from './utils';
 
 let isOpen: boolean;
 
+
+// Reviews
 const reviews: {
     name: string;
     stars: number;
@@ -30,9 +32,6 @@ const reviews: {
     },
 ]
 
-
-totalReviews(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
 const you: {
     firstName : string;
     lastName: string;
@@ -46,5 +45,46 @@ const you: {
     isReturning: true,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
+
+
+
+// Object Types Challenge
+// Based on what we discussed we need to make up our Property Objects and array,
+// can you create that array, making sure to assign the correct Types?
+
+
+// Properties
+const properties: {
+    image: string;
+    title: string;
+    pricePerNight: number;
+    location: {
+        lineAddres: string;
+        townCity: string;
+        postCode: number;
+        country: string;
+    };
+    contact: string;
+    availabilityToRent: boolean;
+}[]=[
+    {
+    image:'png', 
+    title: 'Simnikiwe',
+    pricePerNight: 350,
+    location: {
+        lineAddres: 'Fortgale',
+        townCity: 'Mthatha',
+        postCode: 5104,
+        country: 'South Africa',
+    },
+    contact: 'email address',
+    availabilityToRent: false,
+    },
+]
+
+
+// Functions Call
+
+totalReviews(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
